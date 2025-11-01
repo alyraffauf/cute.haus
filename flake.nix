427d995877ce72d00f0a7ea65bd63b42f72a1ca8
Aly Raffauf <aly@aly.codes>
2025-11-01 14:29:22 -0400

    flake/nixConfig: add cutehaus cachix substituter

--- flake.nix
+++ flake.nix
@@ -161,14 +161,14 @@
     accept-flake-config = true;
 
     extra-substituters = [
-      "https://alyraffauf.cachix.org"
       "https://chaotic-nyx.cachix.org/"
+      "https://cutehaus.cachix.org"
       "https://nix-community.cachix.org"
     ];
 
     extra-trusted-public-keys = [
-      "alyraffauf.cachix.org-1:GQVrRGfjTtkPGS8M6y7Ik0z4zLt77O0N25ynv2gWzDM="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
+      "cutehaus.cachix.org-1:KiifTsseQBitoaHH8rkDUDwzyz9akLeOM+K+e2eK8dA="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
