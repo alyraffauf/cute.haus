3f2a5749ae3e1e70a60cfbadf5a2e141baa59a21
Aly Raffauf <aly@aly.codes>
2025-11-09 08:42:05 -0500

    nixos/profiles/base: remove direnv, gnupg agent

--- modules/nixos/profiles/base/default.nix
+++ modules/nixos/profiles/base/default.nix
@@ -49,18 +49,6 @@
 
     programs = {
       dconf.enable = true; # Needed for home-manager
-
-      direnv = {
-        enable = true;
-        nix-direnv.enable = true;
-        silent = true;
-      };
-
-      gnupg.agent = {
-        enable = true;
-        enableSSHSupport = true;
-      };
-
       nh.enable = true;
       ssh.knownHosts = config.mySnippets.ssh.knownHosts;
     };
