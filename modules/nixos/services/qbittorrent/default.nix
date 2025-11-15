0c4e6879795c005d387ff362cbf29b44238d8d92
Aly Raffauf <aly@aly.codes>
2025-11-15 12:08:52 -0500

    nixos/services/qbittorrent: wrap around nixpkgs module

--- modules/nixos/services/qbittorrent/default.nix
+++ modules/nixos/services/qbittorrent/default.nix
@@ -1,4 +1,3 @@
-# Borrowed graciously from https://github.com/WiredMic/nix-config/commit/d9268ce5190a2041ef66b492900eed278d1508e2#diff-9db90aeeaf81739c27dcdab8065abc8709d0bd5428bc658cff2db46acc91536a
 {
   config,
   lib,
@@ -6,8 +5,6 @@
   ...
 }: let
   cfg = config.myNixOS.services.qbittorrent;
-  UID = 888;
-  GID = 888;
 in {
   options.myNixOS.services.qbittorrent = {
     enable = lib.mkEnableOption "qBittorrent headless";
@@ -33,13 +30,13 @@ in {
     port = lib.mkOption {
       type = lib.types.port;
       default = 8080;
-      description = "qBittorrent web UI port.";
+      description = "qbittorrent web UI port.";
     };
 
     openFirewall = lib.mkOption {
       type = lib.types.bool;
       default = false;
-      description = "Open services.qBittorrent.port to the outside network.";
+      description = "qbittorrent.port to the outside network.";
     };
 
     package = lib.mkOption {
@@ -51,57 +48,18 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    networking.firewall =
-      lib.mkIf cfg.openFirewall {allowedTCPPorts = [cfg.port];};
+    services.qbittorrent = {
+      inherit
+        (config.myNixOS.services.qbittorrent)
+        openFirewall
+        user
+        group
+        package
+        ;
 
-    systemd.services.qbittorrent = {
-      after = ["local-fs.target" "network-online.target"];
-      description = "qBittorrent-nox service";
-      documentation = ["man:qbittorrent-nox(1)"];
-      requires = ["local-fs.target" "network-online.target"];
-      wantedBy = ["multi-user.target"];
-
-      serviceConfig = {
-        Type = "simple";
-        User = cfg.user;
-        Group = cfg.group;
-
-        # Run the pre-start script with full permissions (the "!" prefix) so it
-        # can create the data directory if necessary.
-        ExecStartPre = let
-          preStartScript = pkgs.writeScript "qbittorrent-run-prestart" ''
-            #!${pkgs.bash}/bin/bash
-
-            # Create data directory if it doesn't exist
-            if ! test -d "$QBT_PROFILE"; then
-              echo "Creating initial qBittorrent data directory in: $QBT_PROFILE"
-              install -d -m 0755 -o "${cfg.user}" -g "${cfg.group}" "$QBT_PROFILE"
-            fi
-          '';
-        in "!${preStartScript}";
-
-        ExecStart = "${cfg.package}/bin/qbittorrent-nox";
-        # To prevent "Quit & shutdown daemon" from working; we want systemd to
-        # manage it!
-        #Restart = "on-success";
-        #UMask = "0002";
-        #LimitNOFILE = cfg.openFilesLimit;
-      };
-
-      environment = {
-        QBT_PROFILE = cfg.dataDir;
-        QBT_WEBUI_PORT = toString cfg.port;
-      };
+      enable = true;
+      profileDir = cfg.dataDir;
+      webuiPort = cfg.port;
     };
-
-    users.users = lib.mkIf (cfg.user == "qbittorrent") {
-      qbittorrent = {
-        inherit (cfg) group;
-        uid = UID;
-      };
-    };
-
-    users.groups =
-      lib.mkIf (cfg.group == "qbittorrent") {qbittorrent = {gid = GID;};};
   };
 }
