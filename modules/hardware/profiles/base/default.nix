8e22f447100d00f0b36c79de2089a87431417f19
Aly Raffauf <aly@aly.codes>
2025-11-20 09:31:25 -0500

    hardware: remove unnecessary enablements

--- modules/hardware/profiles/base/default.nix
+++ modules/hardware/profiles/base/default.nix
@@ -6,24 +6,7 @@
   options.myHardware.profiles.base.enable = lib.mkEnableOption "Base common hardware configuration";
 
   config = lib.mkIf config.myHardware.profiles.base.enable {
-    hardware = {
-      enableAllFirmware = true;
-
-      bluetooth = {
-        enable = true;
-        powerOnBoot = true;
-      };
-    };
-
-    services = {
-      fstrim.enable = true;
-
-      logind.settings.Login = {
-        HandlePowerKey = "suspend";
-        HandlePowerKeyLongPress = "poweroff";
-      };
-    };
-
-    zramSwap.enable = lib.mkDefault true;
+    hardware.enableAllFirmware = true;
+    services.fstrim.enable = true;
   };
 }
