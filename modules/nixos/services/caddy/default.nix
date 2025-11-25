46b4588ede1cb6da86ad6e34962bb42577278e2c
Aly Raffauf <aly@aly.codes>
2025-11-25 11:53:26 -0500

    nixos/services/caddy: update hashes

--- modules/nixos/services/caddy/default.nix
+++ modules/nixos/services/caddy/default.nix
@@ -25,7 +25,7 @@
 
         package = pkgs.caddy.withPlugins {
           plugins = ["github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"];
-          hash = "sha256-rI09/CaMtuU3JERFuiXBAzwyvwG5LxO5BRGgTV86+yc=";
+          hash = "sha256-vH3Y5gFAKsCll2Wq3jxQCMWT4G0OwHSGn94y5/eP7zw=";
         };
       };
 
