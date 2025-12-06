ef840ea8d0d14978b2449cee46ffa44ad3c52801
Aly Raffauf <aly@aly.codes>
2025-12-06 10:06:15 -0500

    flake/overlays/qbittorrent-nox: 5.1.2 -> 5.1.4

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -2,13 +2,13 @@ _: {
   flake.overlays = {
     default = _final: prev: {
       qbittorrent-nox = prev.qbittorrent-nox.overrideAttrs (_old: rec {
-        version = "5.1.2";
+        version = "5.1.4";
 
         src = prev.fetchFromGitHub {
           owner = "qbittorrent";
           repo = "qBittorrent";
           rev = "release-${version}";
-          hash = "sha256-2hcG2rMwo5wxVQjCEXXqPLGpdT6ihqtt3HsNlK1D9CA=";
+          hash = "sha256-9RfKir/e+8Kvln20F+paXqtWzC3KVef2kNGyk1YpSv4=";
         };
       });
     };
