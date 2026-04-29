4640a2adc7255d694153bd6be515f205bb9cc583
Aly Raffauf <aly@aly.codes>
2026-04-28 21:06:56 -0400

    solaceon/bluesky-pds: enable goat

--- hosts/solaceon/services.nix
+++ hosts/solaceon/services.nix
@@ -3,6 +3,7 @@
     bluesky-pds = {
       enable = true;
       environmentFiles = [config.age.secrets.pds.path];
+      goat.enable = true;
       pdsadmin.enable = true;
       settings.PDS_HOSTNAME = config.mySnippets.cute-haus.networkMap.aly-social.vHost;
     };
