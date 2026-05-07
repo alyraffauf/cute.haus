d8d799accc5500d5b96ab0daec82e7c2d0e51c4a
Aly Raffauf <aly@aly.codes>
2026-05-07 14:38:15 -0400

    eterna: add k3s secret

--- hosts/eterna/secrets.nix
+++ hosts/eterna/secrets.nix
@@ -1,5 +1,6 @@
 {self, ...}: {
   age.secrets = {
+    k3s.file = "${self.inputs.secrets}/k3s.age";
     rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
     restic-passwd.file = "${self.inputs.secrets}/restic-password.age";
     tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
