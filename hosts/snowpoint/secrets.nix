cddb7bea8bf9bb12ecf6907633764e7c27a2a349
Aly Raffauf <aly@aly.codes>
2026-05-08 00:23:10 -0400

    snowpoint: cleanup secrets

--- hosts/snowpoint/secrets.nix
+++ hosts/snowpoint/secrets.nix
@@ -6,6 +6,5 @@
     syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/snowpoint/cert.age";
     syncthingKey.file = "${self.inputs.secrets}/aly/syncthing/snowpoint/key.age";
     tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
-    vaultwarden.file = "${self.inputs.secrets}/vaultwarden.age";
   };
 }
