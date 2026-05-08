90ef88c830f3d3aedcaa7d32de195ae93ed4d10d
Aly Raffauf <aly@aly.codes>
2026-05-08 09:31:14 -0400

    move secrets out of nix

--- hosts/solaceon/default.nix
+++ hosts/solaceon/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   modulesPath,
-  pkgs,
   self,
   ...
 }: {
@@ -31,39 +30,6 @@
   networking = {
     firewall.allowedTCPPorts = [2222 8282 8383];
     hostName = "solaceon";
-  };
-
-  systemd.services = let
-    mkTlsSync = name: {
-      description = "Sync ${name} origin cert into k8s secret";
-      after = ["k3s.service"];
-      wants = ["k3s.service"];
-      wantedBy = ["multi-user.target"];
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        Restart = "on-failure";
-        RestartSec = 10;
-      };
-
-      script = ''
-        export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-        until ${pkgs.k3s}/bin/k3s kubectl get nodes >/dev/null 2>&1; do
-          sleep 2
-        done
-        ${pkgs.k3s}/bin/k3s kubectl create secret tls ${name}-tls \
-          --cert=${config.age.secrets."${name}-tls-crt".path} \
-          --key=${config.age.secrets."${name}-tls-key".path} \
-          --dry-run=client -o yaml \
-          | ${pkgs.k3s}/bin/k3s kubectl apply -f -
-      '';
-    };
-  in {
-    k3s-aly-codes-tls = mkTlsSync "aly-codes";
-    k3s-aly-social-tls = mkTlsSync "aly-social";
-    k3s-cute-haus-tls = mkTlsSync "cute-haus";
-    k3s-morsels-blue-tls = mkTlsSync "morsels-blue";
   };
 
   nixpkgs.hostPlatform = "x86_64-linux";
--- hosts/solaceon/secrets.nix
+++ hosts/solaceon/secrets.nix
@@ -1,13 +1,5 @@
 {self, ...}: {
   age.secrets = {
-    aly-codes-tls-crt.file = "${self.inputs.secrets}/aly-codes-tls.crt.age";
-    aly-codes-tls-key.file = "${self.inputs.secrets}/aly-codes-tls.key.age";
-    aly-social-tls-crt.file = "${self.inputs.secrets}/aly-social-tls.crt.age";
-    aly-social-tls-key.file = "${self.inputs.secrets}/aly-social-tls.key.age";
-    cute-haus-tls-crt.file = "${self.inputs.secrets}/cute-haus-tls.crt.age";
-    cute-haus-tls-key.file = "${self.inputs.secrets}/cute-haus-tls.key.age";
-    morsels-blue-tls-crt.file = "${self.inputs.secrets}/morsels-blue-tls.crt.age";
-    morsels-blue-tls-key.file = "${self.inputs.secrets}/morsels-blue-tls.key.age";
     k3s.file = "${self.inputs.secrets}/k3s.age";
     rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
     restic-passwd.file = "${self.inputs.secrets}/restic-password.age";
