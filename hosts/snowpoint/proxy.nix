bf12f6f7f156c1fad782519d511412e19f08529f
Aly Raffauf <aly@aly.codes>
2026-05-08 07:41:39 -0400

    snowpoint: drop couchdb

--- hosts/snowpoint/proxy.nix
+++ hosts/snowpoint/proxy.nix
@@ -4,14 +4,6 @@
       email = "alyraffauf@fastmail.com";
 
       virtualHosts = {
-        "${config.mySnippets.tailnet.networkMap.couchdb.vHost}" = {
-          extraConfig = ''
-            bind tailscale/couchdb
-            encode zstd gzip
-            reverse_proxy ${config.mySnippets.tailnet.networkMap.couchdb.hostName}:${toString config.mySnippets.tailnet.networkMap.couchdb.port}
-          '';
-        };
-
         "${config.mySnippets.tailnet.networkMap.navidrome.vHost}" = {
           extraConfig = ''
             bind tailscale/navidrome
