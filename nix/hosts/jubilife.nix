15bad7100d1bbd439d025e052328dd2f63564181
Aly Raffauf <aly@aly.codes>
2026-06-10 18:05:37 -0400

    jubilife: don't spin down drives

--- nix/hosts/jubilife.nix
+++ nix/hosts/jubilife.nix
@@ -47,7 +47,6 @@
       (
         {
           config,
-          lib,
           pkgs,
           ...
         }: let
@@ -101,19 +100,19 @@
             hostName = "jubilife";
           };
 
-          services.udev.extraRules = let
-            mkRule = as: lib.concatStringsSep ", " as;
-            mkRules = rs: lib.concatStringsSep "\n" rs;
-          in
-            mkRules [
-              (mkRule [
-                ''ACTION=="add|change"''
-                ''SUBSYSTEM=="block"''
-                ''KERNEL=="sd[a-z]"''
-                ''ATTR{queue/rotational}=="1"''
-                ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k"''
-              ])
-            ];
+          # services.udev.extraRules = let
+          #   mkRule = as: lib.concatStringsSep ", " as;
+          #   mkRules = rs: lib.concatStringsSep "\n" rs;
+          # in
+          #   mkRules [
+          #     (mkRule [
+          #       ''ACTION=="add|change"''
+          #       ''SUBSYSTEM=="block"''
+          #       ''KERNEL=="sd[a-z]"''
+          #       ''ATTR{queue/rotational}=="1"''
+          #       ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k"''
+          #     ])
+          #   ];
 
           system.stateVersion = "25.11";
           myDisko.installDrive = "/dev/disk/by-id/nvme-PNY_CS2130_1TB_SSD_PNY211821050701050CC";
